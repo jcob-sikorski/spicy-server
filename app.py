5e3bd9a819bace739da264ade06cff5838fd8b98
jcob-sikorski <jm.siekiera@gmail.com>
2023-07-02 13:56:00 +0200

    added logs to server

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, jsonify
 import stripe
 import os
 from dotenv import load_dotenv
+import logging
 
 app = Flask(__name__)
+
+# Create a logger object
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)  # Set log level to INFO. Change it to DEBUG, ERROR, WARN as per your requirement.
 
 # Load environment variables from a .env file
 def configure_secrets():
     load_dotenv()
+    logger.info("Environment Variables Loaded")
 
 configure_secrets()
 
@@ -18,13 +24,18 @@ configure_secrets()
 def payment_sheet():
     # Set your secret key. Remember to switch to your live secret key in production.
     # See your keys here: https://dashboard.stripe.com/apikeys
+
+    logger.info("Got payment request")
+
     stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
       # Use an existing Customer ID if this is a returning customer
     customer = stripe.Customer.create()
+    logger.info("Created Stripe Customer")
     ephemeralKey = stripe.EphemeralKey.create(
         customer=customer['id'],
         stripe_version='2022-11-15',
     )
+    logger.info("Created Stripe ephemeralKey")
     paymentIntent = stripe.PaymentIntent.create(
         amount=2,
         currency='pln',
@@ -33,6 +44,13 @@ def payment_sheet():
           'enabled': True,
         },
     )
+    logger.info("Created Stripe paymentIntent")
+
+    logger.info(f"Returning response: {jsonify(paymentIntent=paymentIntent.client_secret,
+                     ephemeralKey=ephemeralKey.secret,
+                     customer=customer.id,
+                     publishableKey=os.getenv('STRIPE_PUBLISHABLE_KEY')
+    )}")
     return jsonify(paymentIntent=paymentIntent.client_secret,
                      ephemeralKey=ephemeralKey.secret,
                      customer=customer.id,
